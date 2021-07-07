232d8c5f63acdaee47fcb36b91d26ae46e2d8a2c
Dian Tri Budianto <dyanzzz@gmail.com>
2021-07-07 09:07:33 +0700

    membuat thread

--- src/test/kotlin/imu/creative/coroutine/ThreadTest.kt
+++ src/test/kotlin/imu/creative/coroutine/ThreadTest.kt
@@ -1,6 +1,8 @@
 package imu.creative.coroutine
 
 import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.concurrent.thread
 
 class ThreadTest {
 
@@ -8,5 +10,41 @@ class ThreadTest {
     fun testThreadName(){
         val threadName = Thread.currentThread().name
         println("Running in thread \"$threadName\"")
+    }
+
+    // fun test ini adalah main thread
+    // running thread dijalankan secara pararel
+    @Test
+    fun testNewThread(){
+        // thread bawaan java
+        /*
+        // membuat runnable yg akan dijalankan oleh thread
+        val runnable = Runnable{
+            println("Start : ${Date()}")
+            // proses selama 2 detik
+            Thread.sleep(2_000)
+
+            println("Finish : ${Date()}")
+        }
+
+        // membuat thread
+        val thread = Thread(runnable)
+        // menjalankan thread
+        thread.start()
+         */
+
+        // thread bawaan kotlin
+        thread(start = true) {
+            println("Start : ${Date()}")
+            // proses selama 2 detik
+            Thread.sleep(2_000)
+
+            println("Finish : ${Date()}")
+        }
+
+        println("MENUNGGU SELESAI")
+        // tunggu dulu selama 3 detik
+        Thread.sleep(3_000)
+        println("SELESAI")
     }
 }
