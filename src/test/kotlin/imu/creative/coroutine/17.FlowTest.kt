474f97ffabb4b00c3bfe102a14c0e85ba29858a7
Dian Tri Budianto <dyanzzz@gmail.com>
2021-07-09 06:26:37 +0700

    membuat flow, flow operator, flow exception, cancellable flow

---
+++ src/test/kotlin/imu/creative/coroutine/17.FlowTest.kt
@@ -0,0 +1,108 @@
+package imu.creative.coroutine
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import org.junit.jupiter.api.Test
+
+// menggunakan flow
+// sifatnya lazy, belum dijalankan sebelum kita memanggilnya
+// mulai dengan index
+class FlowTest {
+
+    // membuat flow
+    @Test
+    fun testFlow() {
+        // emit() untuk mengirim data ke collect
+        // collect() untuk mengakses/ menerima(receive) data
+
+        // setelah di emit melakukan proses kirim, collect langsung proses terima
+        val flow: Flow<Int> = flow {
+            println("Start flow")
+            repeat(100){
+                println("Emit $it")
+                emit(it)
+            }
+        }
+
+        runBlocking {
+            flow.collect {
+                println("Receive $it")
+            }
+        }
+    }
+
+    // melakukan emit/ mengirim value dari 0-99 ke flow
+    private suspend fun numberFlow(): Flow<Int> = flow {
+        repeat(100){
+            emit(it)
+        }
+    }
+
+    // konversi dari integer ke string
+    private suspend fun changeToString(number: Int): String {
+        delay(100)
+        return "Number $number"
+    }
+
+    // flow operator, untuk mencetak angka hasil bagi 0 yg dimodulus 2 == genap
+    @Test
+    fun testFlowOperator() {
+        runBlocking {
+            val flow = numberFlow()
+            flow.filter { it % 2 == 0 }
+                .map { changeToString(it) }
+                .collect { println(it) }
+        }
+    }
+
+    // flow exception
+    @Test
+    fun testFlowException() {
+        // flow juga mempunyai function catch dan finnaly untuk exceptionHandler seperti try catch, jadi ga perlu bi
+        // catch gunakan .catch()
+        // finally gunakan .onCompletion()
+        runBlocking {
+            numberFlow()
+                .map {
+                    check(it < 10)  // valuenya harus selalu kurang dari 10
+                    // check ini memeriksa, kalau true, tidak ada masalah, tapi jika false
+                    // akan error IllegalStateException karena kondisi sudah tidak sesuai
+                    it  // return it lagi
+                }
+                .onEach {
+                    println(it) // print flownya
+                }
+                .catch {    // catch disini seperty try dan catch
+                    println("Error : ${it.message}")
+                }
+                .onCompletion { // onCompletion disini seperti try dan finally
+                    println("Done")
+                }
+                    // bisa collect menggunakan lambda atau tidak
+                .collect()  // untuk menjalankan flownya, gunakan collect
+
+        }
+    }
+
+    // cancellable flow
+    @Test
+    fun testCancelableFlow() {
+        val scope = CoroutineScope(Dispatchers.IO)
+        runBlocking {
+            val job = scope.launch {
+                numberFlow()
+                    .onEach {
+                        // membatalkan flow itu tinggal membatalkan coroutinenya aja
+                        // maka otomatis coroutine yg menggunakan coroutinenya, tidak akan menggunakan coroutinenya lagi
+                        // cancel() disini milik coroutinenya ya
+                        // bisa juga ditulis dengan this.cancel()
+                        if (it > 10) cancel()
+                        else println(it)
+                    }
+                    .collect()
+            }
+
+            job.join()
+        }
+    }
+}
