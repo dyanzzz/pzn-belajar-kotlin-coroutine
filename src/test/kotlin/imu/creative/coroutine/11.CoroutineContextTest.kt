bdb6f64934b8928f5470aefa8e3c705e8e8df9a0
Dian Tri Budianto <dyanzzz@gmail.com>
2021-07-08 10:45:11 +0700

    merubah nama coroutine context dan menggabungkan coroutine context element

--- src/test/kotlin/imu/creative/coroutine/11.CoroutineContextTest.kt
+++ src/test/kotlin/imu/creative/coroutine/11.CoroutineContextTest.kt
@@ -2,6 +2,7 @@ package imu.creative.coroutine
 
 import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
+import java.util.concurrent.Executors
 import kotlin.coroutines.CoroutineContext
 
 class CoroutineContextTest {
@@ -23,6 +24,63 @@ class CoroutineContextTest {
                 job?.cancel()
                  */
             }
+            job.join()
+        }
+    }
+
+    // jika ingin merubah nama coroutine contextnya
+    // bisa dibedakan antara coroutine parent and childnya
+    @Test
+    fun testCoroutineName() {
+        val scope = CoroutineScope(Dispatchers.IO)
+
+        // bisa menambahkan coroutineName() disini
+        val job = scope.launch(CoroutineName("Parent")) {
+            println("Parent run in thread : ${Thread.currentThread().name}")
+            withContext(CoroutineName("Child")){
+                println("Child run in thread : ${Thread.currentThread().name}")
+            }
+        }
+
+        runBlocking {
+            job.join()
+        }
+    }
+
+    // jika ingin membuat coroutineName parent and childnya sama, bisa di setup di atas ketika membuat coroutineScope, dengan dispatcher
+    @Test
+    fun testCoroutineNameContext() {
+        // bisa menambahkan coroutineName() disini
+        val scope = CoroutineScope(Dispatchers.IO + CoroutineName("TestCoroutineName"))
+
+        val job = scope.launch {
+            println("Parent run in thread : ${Thread.currentThread().name}")
+            withContext(Dispatchers.IO){
+                println("Child run in thread : ${Thread.currentThread().name}")
+            }
+        }
+
+        runBlocking {
+            job.join()
+        }
+    }
+
+    // menggabungkan context element
+    @Test
+    fun testCoroutineElements() {
+        val dispatcher = Executors.newFixedThreadPool(10).asCoroutineDispatcher()
+        // menggabungkan context element
+        val scope = CoroutineScope(Dispatchers.IO + CoroutineName("Test"))
+
+        // bisa menambahkan coroutineName() disini
+        val job = scope.launch(CoroutineName("Parent") + dispatcher) {
+            println("Parent run in thread : ${Thread.currentThread().name}")
+            withContext(CoroutineName("Child") + Dispatchers.IO){
+                println("Child run in thread : ${Thread.currentThread().name}")
+            }
+        }
+
+        runBlocking {
             job.join()
         }
     }
